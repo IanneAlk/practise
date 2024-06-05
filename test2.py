3680aa4edef43d1f77a455bb56d1f57c2c08bd7b
IanneAlk <alukuianijait@gmail.com>
2024-06-05 10:32:03 +0300

    first print prog

---
+++ test2.py
@@ -0,0 +1,5 @@
+#THIS SHOULD PRINT
+print("This is Ian,i have absolutely no idea what am doing,generally ,but here we are.")#THIS SHOULD PRINT
+print("This is Ian,i have absolutely no idea what am doing,generally ,but here we are.")#THIS SHOULD PRINT
+print("This is Ian,i have absolutely no idea what am doing,generally ,but here
+we are.")
